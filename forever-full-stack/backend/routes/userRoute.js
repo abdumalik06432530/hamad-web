f6b74583f8c4e6b0feab4e0b76923bd4f69a5e1c
nurezf <nuredinzf1996@gmail.com>
2025-05-12 14:33:36 +0300

    adjesting backend for use

--- forever-full-stack/backend/routes/userRoute.js
+++ forever-full-stack/backend/routes/userRoute.js
@@ -1,10 +1,16 @@
-import express from 'express';
-import { loginUser,registerUser,adminLogin } from '../controllers/userController.js';
+import express from "express";
+import {
+  loginUser,
+  registerUser,
+  adminLogin,
+  adminRegister,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/register',registerUser)
-userRouter.post('/login',loginUser)
-userRouter.post('/admin',adminLogin)
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.post("/admin", adminLogin);
+userRouter.post("/adminregister", adminRegister);
 
 export default userRouter;
